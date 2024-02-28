d1c20c32851f33732a63b9ee64a2e89bd7bf5535
W1412X <tor108@outlook.com>
2024-02-28 18:54:42 +0800

    new pthiroject

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
 #/bin/bash
+echo "|-> Install the tools(expect)"
+sudo apt update
+sudo apt install expect
+
 echo "|-> Config git"
 read -p "Enter the Github Username:" username
 read -p "Enter the token you get:" token
