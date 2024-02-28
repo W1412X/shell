9c6522ab7ef10c06c4e8bd2adf0e7a5e15023bca
W1412X <tor108@outlook.com>
2024-02-28 17:32:05 +0800

    28-17-32

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #/bin/bash
+echo "|-> Config git"
+git config 
 read -p "Enter the Github Username:" username
 read -p "Enter the token you get:" token
 echo "$username" > ./.W_GITPUSH_TOKEN
@@ -6,3 +8,6 @@ echo "$token" >> ./.W_GITPUSH_TOKEN
 sudo cp ./.W_GITPUSH_TOKEN ~/.W_GITPUSH_TOKEN
 sudo cp ./git_push.sh /usr/bin/git_push
 sudo chmod +x /usr/bin/git_push
+read -p "Enter the Github Usermail:" usermail 
+git config --global user.email "$usermail"
+git config --global user.name "$username"
