37e2874a0cabd78c01715ea1077b8e8214a46c10
W1412X <tor108@outlook.com>
2024-02-28 17:17:04 +0800

    new pthiroject

--- git_push.sh
+++ git_push.sh
@@ -35,22 +35,22 @@ if [ -d ".git" ]; then
         exit 1
     fi
     echo "|-> Try push start"
-    git add . >> log
-    git commit -m "$comment" >> log
-    git branch -M $branch >> log
-    git remote add origin "https://github.com/$username/$repo_name.git" >> log
-    git push origin main >> log
+    git add . >> gitpushlog
+    git commit -m "$comment" >> gitpushlog
+    git branch -M $branch >> gitpushlog
+    git remote add origin "https://github.com/$username/$repo_name.git" >> gitpushlog
+    git push origin main >> gitpushlog
     echo "|-> Push OK"
 else
     echo "|-> This catagory is not a repository,will init it"
     echo "|-> Init it"
-    git init >> log
-    git add . >> log
-    git commit -m "$comment" >> log
+    git init >> gitpushlog
+    git add . >> gitpushlog
+    git commit -m "$comment" >> gitpushlog
     echo "|-> Create it on Github"
-    curl -k -u "$username:$token" https://api.github.com/user/repos -d '{"name":"'$repo_name'"}' >> log
+    curl -k -u "$username:$token" https://api.github.com/user/repos -d '{"name":"'$repo_name'"}' >> gitpushlog
     echo "|-> Push"
-    git branch -M main >> log
-    git remote add origin "https://github.com/$username/$repo_name.git" >> log
-    git push origin main >> log
+    git branch -M main >> gitpushlog
+    git remote add origin "https://github.com/$username/$repo_name.git" >> gitpushlog
+    git push origin main >> gitpushlog
 fi
